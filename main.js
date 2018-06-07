99d1f9b292f632a524aaec03af396cfed13075d5
Mark Vonk <markjhvonk@gmail.com>
2018-06-07 19:09:11 +0200

    recoded most of the 'algorithm' added drawn line to map

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var jsonData;
 var boatMarker;
 
 var boatPos = [52.373, 4.874]; // [lat, lon]
-
+var points = new Array();
 var route = new Array();    // create route array
 
 
@@ -31,6 +31,8 @@ function initMap() {
     
     var newLat = boatPos[0];
     var newLon = boatPos[1];
+
+    routeData();
 
     function loop() {
         // console.log("boat lat: "+newLat+" lon: "+newLon)
@@ -58,117 +60,116 @@ function initMap() {
 
 // 1.
 // fetch dockPoints data
-fetch('./data/dock-points.json')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        calculate(data); // call calculate function when the data is received
-        console.log(route)
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-
+function routeData(){
+    var routeResult;
+    fetch('./data/dock-points.json')
+    .then(
+        function(response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+                return;
+            }
+            // Examine the text in the response
+            response.json().then(function(data) {
+                routeResult = calculateRoute(data); // call calculate function when the data is received
+                drawPath(routeResult);
+            });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
 
 // 2.
 // calculate function to handle point to point calculation
-function calculate(data) {
+function calculateRoute(data) {
     // get boat position
     var lat2 = boatPos[0];
     var lon2 = boatPos[1];
 
+    // push the boat to the route array as starting point
     route.push(["boat", 0, lat2, lon2])
 
-    var points = data.points;   // get the points data in own array
-    var distances = new Array();// make temporary array for the distances
-    // points.shift();
-    // console.log(points);
+    // push the points from the json file to a new array
+    // this format is easier to use later on in the code
+    for(let point of data.points){
+        let location = new Array();
+        location[0] = point.title;
+        // leave location[1] clear for the distance
+        location[2] = point.lat;
+        location[3] = point.lon;
+        points.push(location);
+    }
     
-    // 2. loop through points and calculate distance between points and boat
-    var i = 0;
-    for(const point of points){
-        console.log(point.lat)
+    // 2. & 3. loop through points and calculate distance between last route item
+    while(points.length > 0){
+        var lastRouteItem = route.length - 1; 
+        var nextPoint = calcPoint(route[lastRouteItem]);
+        // 2.1 add the nearest point to the previous point to the route array
+        route.push(nextPoint);
+    }
+
+    // log the final route
+    console.log("Route: ");
+    console.log(route);
+
+    return route;
+}
+
+
+// function that takes the current point and returns the next, 
+// closest point, to itself from the remaining points in the pointsArray (2.1)
+// afterwards it removes this point from the pointsArray (2.2)
+function calcPoint(currentPoint) {
+    var distances = new Array(); // make array for the distances
+    for(let point of points){
+
         // call distance function
-        var distance = getDistance(point.lat, point.lon, lat2, lon2);
+        var distance = getDistance(point[2], point[3], Number(currentPoint[2]), Number(currentPoint[3]));
 
         // put the distance from the boat to the point in an array
         var location = new Array();
-        location[0] = point.title;
-        location[1] = Number(distance)*1000; // *1000 to get meters instead of km's
-        location[2] = point.lat;
-        location[3] = point.lon;
-        distances.push(location);
-
-        var arrayPos = i + 1;
-        if(arrayPos == points.length){
-            // from here the next points should also be calculated
-            distances.sort(compareSecondColumn)
-            route.push(distances[0]);
-            distances.shift();
-            console.log("route: ")
-            console.log(route)
-            console.log("distances: ")
-            console.log(distances)
-            // nextPoint(distances, route)
-        }
-
-        i++;
+        location[0] = point[0];
+        location[1] = Number(distance)*1000; // *1000 to get meters instead of km's     
+        location[2] = point[2];
+        location[3] = point[3];
+        distances.push(location); // add current position and distance to boat to distances array
     }
 
+    distances.sort(compareSecondColumn); // sort the array by the distances
+
+    var closestPoint = distances[0];   // take the lowest distance
+
+    distances.shift(); // 2.2 remove the used point from the distances
+    points = distances; // and replace the points array with the remaining items of the distances array
+
+    return closestPoint; // return the closest point
 }
 
-function nextPoint(distances, route) {
-    var tempDistances = new Array();
-    //last route point position
-    var lastRouteNum = route.length - 1;
-    var lastRoutePoint = route[lastRouteNum];
-    var lat2 = lastRoutePoint[2];
-    var lon2 = lastRoutePoint[3];
+// draw a line based on coordinates
+function drawPath(routeResult) {
+    var routePlanCoordinates = new Array();
 
-    if(tempDistances.length <= 1) {
-        route.push(tempDistances);
-    } else {
-        for(var i = 0; i < distances.length; i++){
-            // get point coordinations
-            var lat1 = distances[i][2];
-            var lon1 = distances[i][3];
-
-            // call distance function
-            var distance = getDistance(lat1, lon1, lat2, lon2);
-
-            // put the distance from the boat to the point in an array
-            var location = new Array();
-            location[0] = distances[i][0];
-            location[1] = Number(distance)*1000; // *1000 to get meters instead of km's
-            location[2] = lat1;
-            location[3] = lon1;
-            tempDistances.push(location);
-
-            var arrayPos = i + 1;
-            if(arrayPos == distances.length){
-                tempDistances.sort(compareSecondColumn)
-                route.push(tempDistances[i]);
-                tempDistances.shift();
-                console.log("TempDistances:");
-                console.log(tempDistances);
-                nextPoint(tempDistances, route)
-            }
-        }
+    // add coordinates to array readable by Google maps JavaScript API
+    for(var location of routeResult){
+        routePlanCoordinates.push({lat: location[2], lng: location[3]})
     }
+    
+    // API settings
+    var routePath = new google.maps.Polyline({
+        path: routePlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    routePath.setMap(map); // add to map
 }
 
-function compareArray(array, currentGeo){
 
-}
+
 
 // function to sort multidimensional array for the second column
 function compareSecondColumn(a, b) {
@@ -190,4 +191,3 @@ function getDistance(lat1, lon1, lat2, lon2) {
   
     return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
-
